4aa1cabcae428d016be6f2bddbaa7b2593ce302b
luoqian <szuluoqian@163.com>
2020-02-20 04:11:17 +0800

    feat(test): add test file

--- test/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { sayHi } from '../src/index';
-
-it('return same value', () => {
-  expect(sayHi('hello')).toBe('hello');
-});
---
+++ test/useEvent.ts
@@ -0,0 +1,8 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useEvent } from '../src/';
+
+describe('useEvent', () => {
+  it('should be defined', () => {
+    expect(useEvent).toBeDefined();
+  });
+});
